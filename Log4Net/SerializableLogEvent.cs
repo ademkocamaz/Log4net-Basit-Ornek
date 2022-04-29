a020520c06ce8133b7a8b1ddbfc2238b85b6f1ee
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-04-29 04:14:13 +0300

    Json Format added

---
+++ Log4Net/SerializableLogEvent.cs
@@ -0,0 +1,21 @@
+﻿using log4net.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net
+{
+    public class SerializableLogEvent
+    {
+        private LoggingEvent loggingEvent;
+
+        public SerializableLogEvent(LoggingEvent loggingEvent)
+        {
+            this.loggingEvent = loggingEvent;
+        }
+        public string UserName => loggingEvent.UserName;
+        public object MessageObject => loggingEvent.MessageObject;
+    }
+}
